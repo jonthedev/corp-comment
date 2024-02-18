b853390e8eee6abbe52c4888653297aacc9c6fb7
Jonathan D Kaonga <jonthedev0@gmail.com>
2024-02-18 22:11:08 +0100

    style: add FeedbackForm styles
    feat: create FeedbackForm template

--- src/components/FeedbackForm/FeedbackForm.tsx
+++ src/components/FeedbackForm/FeedbackForm.tsx
@@ -1,5 +1,23 @@
 import styles from "./FeedbackForm.module.css"
 
 export const FeedbackForm = () => {
-  return <form className={styles["feedback-form"]}>FeedbackForm</form>
+  return (
+    <form className={styles["feedback__form"]}>
+      {" "}
+      <textarea
+        id="feedback-textarea"
+        placeholder="blabla"
+        spellCheck={false}
+      />
+      <label htmlFor="feedback-textarea">
+        Enter your feedback here, remember to #hashtag the company
+      </label>
+      <div>
+        <p className="u-italic">Count</p>
+        <button>
+          <span>Submit</span>
+        </button>
+      </div>
+    </form>
+  )
 }
