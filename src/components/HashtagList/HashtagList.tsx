e82cf99753276ae157432925ef3464ae4973072d
Jonathan D Kaonga <jonthedev0@gmail.com>
2024-02-18 19:36:16 +0100

    feat: add HashtagList buttons

--- src/components/HashtagList/HashtagList.tsx
+++ src/components/HashtagList/HashtagList.tsx
@@ -1,5 +1,17 @@
 import styles from "./HashtagList.module.css"
 
 export const HashtagList = () => {
-  return <ul className={styles["hashtags"]}>HashtagList</ul>
+  return (
+    <ul className={styles["hashtags"]}>
+      <li>
+        <button>#JDev</button>
+      </li>
+      <li>
+        <button>#Nike</button>
+      </li>
+      <li>
+        <button>#Nandos</button>
+      </li>
+    </ul>
+  )
 }
