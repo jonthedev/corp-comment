8b4f72fd7dee0acacfb39e26c697054f4ac5bc60
Jonathan D Kaonga <jonthedev0@gmail.com>
2024-02-19 21:32:20 +0100

    feat: create Spinner component for loading state
    feat: add Spinner to loading state

--- src/components/FeedbackList/FeedbackList.tsx
+++ src/components/FeedbackList/FeedbackList.tsx
@@ -2,22 +2,27 @@ import { useEffect, useState } from "react"
 import styles from "./FeedbackList.module.css"
 import { type FeedbackItem, FeedbackListItem } from "./FeedbackListItem"
 import { URL_COMMENTS } from "../../lib/consts"
+import { Spinner } from "../Spinner/Spinner"
 
 export const FeedbackList = () => {
   const [feedbackItems, setFeedbackItems] = useState<FeedbackItem[]>([])
+  const [isLoading, setIsLoading] = useState<boolean>(false)
 
   useEffect(() => {
+    setIsLoading(true)
     fetch(URL_COMMENTS)
       .then((response) => {
         return response.json()
       })
       .then((data) => {
         setFeedbackItems(data)
+        setIsLoading(false)
       })
   }, [])
 
   return (
     <ol className={styles["feedback-list"]}>
+      {isLoading ? <Spinner /> : null}
       {feedbackItems.map((feedbackItem) => (
         <FeedbackListItem key={feedbackItem.id} feedbackItem={feedbackItem} />
       ))}
