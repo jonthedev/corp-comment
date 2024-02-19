5a3ddbc986c5864f7e22abbde6634a3c6a26aaba
Jonathan D Kaonga <jonthedev0@gmail.com>
2024-02-19 14:36:00 +0100

    refactor: add event type for handleOnChange in FeedbackForm
    refactor: add type to text state in FeedbackForm

--- src/components/FeedbackForm/FeedbackForm.tsx
+++ src/components/FeedbackForm/FeedbackForm.tsx
@@ -1,10 +1,18 @@
-import { useState } from "react"
+import React, { useState } from "react"
 import styles from "./FeedbackForm.module.css"
 import { MAX_CHARACTERS } from "../../lib/consts"
 
 export const FeedbackForm = () => {
-  const [text, setText] = useState("")
+  const [text, setText] = useState<string>("")
   const charCount = MAX_CHARACTERS - text.length
+
+  const handleOnChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const newText = event.target.value
+    if (newText.length > MAX_CHARACTERS) {
+      return
+    }
+    setText(event.target.value)
+  }
 
   return (
     <form className={styles["feedback__form"]}>
@@ -14,7 +22,7 @@ export const FeedbackForm = () => {
         placeholder="blabla"
         spellCheck={false}
         value={text}
-        onChange={(e) => setText(e.target.value)}
+        onChange={handleOnChange}
       />
       <label htmlFor="feedback-textarea">
         Enter your feedback here, remember to #hashtag the company
