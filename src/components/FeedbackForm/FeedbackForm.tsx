31f245f269460cb4bfdff36a75e90a943f1f5663
Jonathan D Kaonga <jonthedev0@gmail.com>
2024-02-19 13:48:23 +0100

    feat: add controlled input functionality to feedback textarea

--- src/components/FeedbackForm/FeedbackForm.tsx
+++ src/components/FeedbackForm/FeedbackForm.tsx
@@ -1,6 +1,11 @@
+import { useState } from "react"
 import styles from "./FeedbackForm.module.css"
+import { MAX_CHARACTERS } from "../../lib/consts"
 
 export const FeedbackForm = () => {
+  const [text, setText] = useState("")
+  const charCount = MAX_CHARACTERS - text.length
+
   return (
     <form className={styles["feedback__form"]}>
       {" "}
@@ -8,12 +13,14 @@ export const FeedbackForm = () => {
         id="feedback-textarea"
         placeholder="blabla"
         spellCheck={false}
+        value={text}
+        onChange={(e) => setText(e.target.value)}
       />
       <label htmlFor="feedback-textarea">
         Enter your feedback here, remember to #hashtag the company
       </label>
       <div>
-        <p className="u-italic">Count</p>
+        <p className="u-italic">{charCount}</p>
         <button>
           <span>Submit</span>
         </button>
