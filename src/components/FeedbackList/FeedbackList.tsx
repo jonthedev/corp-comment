7bf9594d672fb316175e680394824f2be7c58806
Jonathan D Kaonga <jonthedev0@gmail.com>
2024-02-19 11:18:07 +0100

    feat: create feedback template

--- src/components/FeedbackList/FeedbackList.tsx
+++ src/components/FeedbackList/FeedbackList.tsx
@@ -1,5 +1,24 @@
 import styles from "./FeedbackList.module.css"
 
 export const FeedbackList = () => {
-  return <ol className={styles["feedback-list"]}>FeedbackList</ol>
+  return (
+    <ol className={styles["feedback-list"]}>
+      <li className={styles["feedback"]}>
+        <button></button>
+        <div>
+          <p>B</p>
+        </div>
+
+        <div>
+          <p>J Dev</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique,
+            excepturi odio dicta vitae nostrum vero dignissimos quidem delectus
+            fugit, officia quo iste eos.
+          </p>
+        </div>
+        <p>4D</p>
+      </li>
+    </ol>
+  )
 }
