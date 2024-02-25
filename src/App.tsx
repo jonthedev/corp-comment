574e8dcab56c7690cc4159ec47f3be652f6bc689
Jonathan D Kaonga <jonthedev0@gmail.com>
2024-02-25 17:50:24 +0100

    chore: add zustand for state management
    refactor: FeedbackItemsContextProvider data to use zustand

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,23 @@ import styles from "./App.module.css"
 import { Container } from "./components/layout/Container/Container"
 import { Footer } from "./components/layout/Footer/Footer"
 import { HashtagList } from "./components/HashtagList/HashtagList"
-import { FeedbackItemsContextProvider } from "./context/FeedbackItems/FeedbackItemsContextProvider"
+import { useFeedbackItemsStore } from "./store/FeedbackItemsStore"
+import { useEffect } from "react"
 
 function App() {
+  const fetchFeedbackItems = useFeedbackItemsStore(
+    state => state.fetchFeedbackItems
+  )
+
+  useEffect(() => {
+    fetchFeedbackItems()
+  }, [fetchFeedbackItems])
+
   return (
     <div className={styles["app"]}>
       <Footer />
-
-      <FeedbackItemsContextProvider>
-        <Container />
-        <HashtagList />
-      </FeedbackItemsContextProvider>
+      <Container />
+      <HashtagList />
     </div>
   )
 }
